f79682164117142ad23569d991bb6b8d7b37aadd
Dan Kowalski <d.kowalski.ski@gmail.com>
2017-10-31 16:00:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var APP_ID = undefined; // replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+var CLIENT_ID     = '3MVG9M6Iz6p_Vt2wn4hwnXnRv.1XpOb._mn0cBUGMnOlcbInybYdbBu4XKxwwt8HTp8ixv0hHa0u28zp1hoHE';
+var CLIENT_SECRET = '4609070234326421070';
+var USERNAME      = 'salesforceoperations@paylinkdirect.com';
+var PASSWORD      = 'Paylink123!!!Yuj0CGcPrvzRwUJWprihJKOY';
+var CALLBACK_URL  = 'http://localhost:3000j/oauth/_callback';
+var ENVIRONMENT   = 'sandbox';
+
+var AlexaSkill = require('./AlexaSkill');
+var nforce     = require('nforce');
+var _          = require('lodash');
+var moment     = require('moment-timezone');
+var pluralize  = require('pluralize');
+
+var Salesforce = function(){
+  AlexaSkill.call(this, APP_ID);
+};
+
+var org = nforce.createConnection({
+  clientId:     CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  redirectUri:  CALLBACK_URL,
+  mode:         'single',
+  environment:  ENVIRONMENT
+});
+
+// Extend AlexaSkill
+Salesforce.prototype = Object.create(AlexaSkill.prototype);
+Salesforce.prototype.constructor = Salesforce;
+
+Salesforce.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("Salesforce onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+Salesforce.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("Salesforce onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+};
+
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+Salesforce.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("Salesforce onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+Salesforce.prototype.intentHandlers = {
+  SearchAccountIntent: function(intent, session, response){
+    handleSearchAccountIntent(response);
+  }
+};
+
+function handleSearchAccountIntent(response){
+  var query = 'SELECT name, id FROM Account LIMIT 1';
+
+  org.authenticate({ username: USERNAME, password: PASSWORD }).then(function(){
+    return org.query({ query: query })
+  }).then(function(results) {
+    var speechOutput = "Sorry, There are no Accounts in your system.";
+    if (results.records.length > 0) {
+      var acc = results.records[0];
+      speechOutput = "I found an Account named: " + acc.get('Name');
+    }
+    response.tellWithCard(speechOutput, "Salesforce", speechOutput);
+  }).error(function(err) {
+    var errorOutput = "Darn, there was a Salesforce problem, sorry";
+    response.tell(errorOutput, "Salesforce", errorOutput);
+  });
+}
+
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the Salesforce skill.
+    var salesforce = new Salesforce();
+    salesforce.execute(event, context);
+};
