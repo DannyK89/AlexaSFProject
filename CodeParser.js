ba26edd1b8cc967571a174f3ffe0144133d6ebe3
Dan Kowalski <d.kowalski.ski@gmail.com>
2017-11-01 16:23:23 -0500

    More work on the code parser

--- CodeParser.js
+++ CodeParser.js
@@ -5,7 +5,59 @@
 */
 
 module.exports.parseString = function(stringSpoken) {
-  var codeSnipit = stringSpoken;
+
+  var codeSnipit = "Could not parse";
+  var Spoken = stringSpoken.toString();
+
+  if(Spoken.indexOf("if") > -1){
+    codeSnipit = if_method(Spoken);
+  }
+  else if(Spoken.indexOf("for") > -1){
+    codeSnipit = for_method(Spoken);
+  }
+  else if(Spoken.indexOf("while") > -1){
+    codeSnipit = "while(){}";
+  }
+  else if(Spoken.indexOf("do") > -1){
+    codeSnipit = "do{}while()";
+  }
 
   return codeSnipit;
+}
+
+function if_method(spokenString){
+  var stringArray = spokenString.split(" ");
+  var returnString = "if(){}";
+
+  return returnString;
+}
+
+function for_method(spokenString){
+
+}
+
+function while_method(spokenString){
+
+}
+
+function do_method(spokenString){
+
+}
+
+function symbolParse(symbol){
+  if(symbol == "greater"){
+    return ">"
+  }
+  else if(symbol == "lesser"){
+    return "<"
+  }
+  else if(symbol == "equal"){
+    return "="
+  }
+  else if(symbol == "and"){
+    return "&&"
+  }
+  else if(symbol == "or"){
+    return "||"
+  }
 }
